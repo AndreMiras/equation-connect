ad1ad7c834f8c326ea1a2ecd8b5111495d24d3a5
Andre Miras <andre.miras@gmail.com>
2021-11-26 10:57:45 +0100

    :sparkles: Simple Bootstrap setup

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import React, { FunctionComponent } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+const Header = (): JSX.Element => (
+  <Navbar bg="dark" variant="dark" expand="sm">
+    <Container>
+      <Navbar.Brand href={process.env.PUBLIC_URL}>
+        <FontAwesomeIcon icon={["fas", "thermometer-three-quarters"]} />{" "}
+        Equation Connect
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Nav className="mr-auto">
+          <Nav.Link href="https://github.com/AndreMiras/equation-connect">
+            <FontAwesomeIcon icon={["fab", "github-alt"]} /> About
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+);
+
+export default Header;
