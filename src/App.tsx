e9d9a51d3f7e648a97be5c3bc37b8e0902628971
Andre Miras <andre.miras@gmail.com>
2021-11-26 11:51:21 +0100

    :white_check_mark: Fixes tests and updates README.md

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-import { Container, Col, Row } from "react-bootstrap";
 import Header from "./components/Header";
 
 library.add(fas, fab);
