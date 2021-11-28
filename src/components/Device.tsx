08dc2da08ff8f85330910a54914b38cf26a89a11
Andre Miras <andre.miras@gmail.com>
2021-11-28 17:12:42 +0100

    :lipstick: Device Accordion (hide Debug)

--- src/components/Device.tsx
+++ src/components/Device.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Card } from "react-bootstrap";
+import { Accordion } from "react-bootstrap";
 import { getDevice } from "../utils/firebase";
 import { DeviceType } from "../types";
 
@@ -18,32 +18,25 @@ const Device = (): JSX.Element => {
   return device === null ? (
     <div />
   ) : (
-    <>
-      <Card className="mb-3">
-        <Card.Body>
-          <Card.Title>{device.data.name}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            {device.data.temp}&deg;
-          </Card.Subtitle>
-          <Card.Text>
-            <ul>
-              <li>id: {id}</li>
-              <li>temp_calc: {device.data.temp_calc}&deg;</li>
-              <li>temp_probe: {device.data.temp_probe}&deg;</li>
-            </ul>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-
-      <Card className="mb-3">
-        <Card.Body>
-          <Card.Title>Debug</Card.Title>
-          <Card.Text>
-            <pre>{JSON.stringify(device, null, 2)}</pre>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </>
+    <Accordion className="mb-3" defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>{device.data.name}</Accordion.Header>
+        <Accordion.Body>
+          <ul>
+            <li>id: {id}</li>
+            <li>temp: {device.data.temp}&deg;</li>
+            <li>temp_calc: {device.data.temp_calc}&deg;</li>
+            <li>temp_probe: {device.data.temp_probe}&deg;</li>
+          </ul>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Debug</Accordion.Header>
+        <Accordion.Body>
+          <pre>{JSON.stringify(device, null, 2)}</pre>
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
   );
 };
 
