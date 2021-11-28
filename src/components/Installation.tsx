098ad17d93c3a702b27984e4735b0f9135a0bcfd
Andre Miras <andre.miras@gmail.com>
2021-11-28 14:24:34 +0100

    :sparkles: Weather widget
    Consumes the `REACT_APP_OPEN_WEATHER_API_KEY` environment variable
    https://openweathermap.org/

--- src/components/Installation.tsx
+++ src/components/Installation.tsx
@@ -1,5 +1,7 @@
 import React, { FunctionComponent } from "react";
-import { Card } from "react-bootstrap";
+import { Alert, Card } from "react-bootstrap";
+// @ts-ignore
+import ReactWeather, { useOpenWeather } from "react-open-weather";
 import ZonesOverview from "./ZonesOverview";
 import { InstallationType } from "../types";
 
@@ -7,18 +9,47 @@ const Installation: FunctionComponent<InstallationType> = ({
   name,
   power,
   location,
+  latitude,
+  longitude,
   zones,
-}) => (
-  <div>
-    <Card className="mb-3">
-      <Card.Body>
-        <Card.Title>{name}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">{location}</Card.Subtitle>
-        <Card.Text>TODO: weather widget</Card.Text>
-      </Card.Body>
-    </Card>
-    <ZonesOverview zones={zones} />
-  </div>
-);
+}) => {
+  const { data, isLoading, errorMessage } = useOpenWeather({
+    key: process.env.REACT_APP_OPEN_WEATHER_API_KEY,
+    lat: latitude.toString(),
+    lon: longitude.toString(),
+    lang: "en",
+    unit: "metric",
+  });
+  const missingApiKeyMessage = (
+    <Alert variant="warning">
+      REACT_APP_OPEN_WEATHER_API_KEY environment variable missing.
+    </Alert>
+  );
+  return (
+    <div>
+      <Card className="mb-3">
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Card.Text>
+            {"REACT_APP_OPEN_WEATHER_API_KEY" in process.env ? (
+              <ReactWeather
+                isLoading={isLoading}
+                errorMessage={errorMessage}
+                data={data}
+                lang="en"
+                locationLabel={location}
+                unitsLabels={{ temperature: "C", windSpeed: "Km/h" }}
+                showForecast
+              />
+            ) : (
+              missingApiKeyMessage
+            )}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <ZonesOverview zones={zones} />
+    </div>
+  );
+};
 
 export default Installation;
