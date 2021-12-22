f55e498fb0f23c096e45fd7d4906d760cc5fcfc3
Andre Miras <andre.miras@gmail.com>
2021-12-22 10:00:32 +0100

    :white_check_mark: feat: test ZoneOverview
    Test the simple scenario

---
+++ src/components/ZoneOverview.test.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { HashRouter as Router } from "react-router-dom";
+import { render } from "@testing-library/react";
+import ZoneOverview from "./ZoneOverview";
+
+const zoneProps = {
+  comfort: 22,
+  devices: {
+    D89DDF3A7D80D89DDF3A7D80: true,
+    F9097F334FC4F9097F334FC4: true,
+  },
+  eco: 16,
+  id: "-MpGOkgR4zExLBUDUafq",
+  mode: "manual",
+  name: "Living room",
+  power: true,
+  temp: 21,
+  type: "radiator",
+};
+
+test("ZoneOverview renders correctly", () => {
+  const { asFragment } = render(
+    <Router>
+      <ZoneOverview zone={zoneProps} />
+    </Router>
+  );
+  expect(asFragment()).toMatchSnapshot();
+});
